92e7a224b133b27f6e93d905e3c3d4f74f3dd650
VUIDART Sylvain <svuidart@gmail.com>
2023-08-29 15:12:58 +0200

    add DateHistogram aggregation

---
+++ src/Aggregations/DateHistogram.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Spatie\ElasticsearchQueryBuilder\Aggregations;
+
+use Spatie\ElasticsearchQueryBuilder\AggregationCollection;
+use Spatie\ElasticsearchQueryBuilder\Aggregations\Concerns\WithAggregations;
+
+class DateHistogram extends Aggregation
+{
+
+    use WithAggregations;
+
+    protected string $field;
+    protected string $calendarInterval;
+    protected string $fixedInterval;
+
+    public static function create(string $name, string $field, $calendarInterval = null, $fixedInterval = null, ...$aggregations): self
+    {
+        return new self($name, $field, $calendarInterval, $fixedInterval, ...$aggregations);
+    }
+
+    public function __construct(string $name, string $field, $calendarInterval = null, $fixedInterval = null, ...$aggregations)
+    {
+        $this->name = $name;
+        $this->field = $field;
+        $this->calendarInterval = $calendarInterval;
+        $this->fixedInterval = $fixedInterval;
+
+        $this->aggregations = new AggregationCollection(...$aggregations);
+    }
+
+    public function payload(): array
+    {
+        $parameters = [
+            'field' => $this->field,
+        ];
+
+        if($this->calendarInterval){
+            $parameters['calendar_interval'] = $this->calendarInterval;
+        }
+        if($this->fixedInterval){
+            $parameters['fixed_interval'] = $this->fixedInterval;
+        }
+
+        $toReturn = [
+            'date_histogram' => $parameters,
+        ];
+
+        if(!$this->aggregations->isEmpty()){
+            $toReturn['aggs'] = $this->aggregations->toArray();
+        }
+
+        return $toReturn;
+    }
+}
